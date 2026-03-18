f0b57978f34b76956dbfdda57c2acb18767d20df
kmiyazaki-pixel <k_miyazaki@vital-area.com>
2026-03-18 16:53:58 +0900

    Add files via upload

---
+++ othello/src/main/java/com/othello/WebSocketConfig.java
@@ -0,0 +1,21 @@
+package com.othello;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.*;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    private final RoomWebSocketHandler handler;
+
+    public WebSocketConfig(RoomWebSocketHandler handler) {
+        this.handler = handler;
+    }
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(handler, "/ws/room")
+                .setAllowedOrigins("*");
+    }
+}
